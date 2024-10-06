6598fd83ead88b02b19a695cbf081f7420291e43
Joel Ahlström <joelahlstrom@outlook.com>
2024-10-06 20:14:01 +0200

    Added nuget package at right place, fixed some entities

--- Domain/Entities/Director.cs
+++ Domain/Entities/Director.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
     public class Director
     {
-        private int DirectorID { get; set; }
-        private string DirectorFirstName { get; set; }
-        private string DirectorLastName { get; set; }
+        public ObjectId Id { get; set; }
+        public string DirectorFirstName { get; set; }
+        public string DirectorLastName { get; set; }
 
         public Director(string directorFirstName, string directorLastName)
         {
