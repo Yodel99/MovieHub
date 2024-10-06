562998c56c3541065e06a9a1868c6c782dd2b25c
Joel Ahlström <joelahlstrom@outlook.com>
2024-10-06 17:07:57 +0200

    Added a few entities and removed unessacary classes

---
+++ Domain/Enums/Genres.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum Genres
+    {
+        Action,
+        Adventure,
+        Fantasy,
+        Horror,
+        SciFi,
+        Romance,
+        Family,
+        Kid,
+        Teen,
+        Adult,
+        Indie,
+    }
+}
