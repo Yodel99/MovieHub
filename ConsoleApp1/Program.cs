3840172e60c3013e0c19894f9b257138c922f3ae
jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-10-07 21:28:48 +0200

    Co-authored-by: Joel Ahlstrom <joelahlstrom@outlook.com>

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Domain.Entities;
+using Domain.Enums;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            // TEst test
+            //Använd anslutningssträngen för att skapa MovieHubContext
+            var db = MovieHubContext.Create();
+         
+         Movie movie = new Movie("Offside", "Gäng grabbar som spelar boll", 200, 8);
+         
+         db.Add(movie);
+         
+         db.SaveChanges();
+         
+
+        }
+    }
+}
